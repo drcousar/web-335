de5a479a93308587deadce33436862c3fce4c532
Donald Cousar <drcousar@yahoo.com>
2019-06-23 17:51:35 -0500

    Add Assignment 8.3

---
+++ week-8/cousar_calculator.py
@@ -0,0 +1,26 @@
+#============================================
+# Title:  Assignment 8.3
+# Author: Don Cousar
+# Date:   23 June 2019
+# Description: Calculator app
+#===========================================
+
+# Add function
+def add(first,second):
+    answer = (first + second)
+    return answer
+
+# Subtract function
+def subtract(first,second):
+    answer = (first - second)
+    return answer
+
+# Divide function
+def divide(first,second):
+    answer = (first / second)
+    return answer
+
+# Body - print results using each function above
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
---
+++ week-8/cousar_hello_world.py
@@ -0,0 +1,7 @@
+#============================================
+# Title:  Assignment 8.2
+# Author: Don Cousar
+# Date:   23 June 2019
+# Description: Display Hello World 
+#===========================================
+print('Hello World')
