197ab6f5c1e0a514c993227b9eb2972d8650c0ad
Donald Cousar <drcousar@yahoo.com>
2019-06-29 19:13:12 -0500

    add Assignment 9.2

---
+++ week-9/cousar-user-service.py
@@ -0,0 +1,34 @@
+#============================================
+# Title:  Assignment 9.2
+# Author: Don Cousar
+# Date:   29 June 2019
+# Description: Querying and Creating Documents
+#===========================================
+# Imports
+
+from pymongo import MongoClient
+import pprint
+import datetime
+
+# Connect to local MongoDB
+client = MongoClient('localhost', 27017)
+
+print(client)
+
+db = client.web335
+
+user = {
+
+    "first_name": "Claude",
+    "last_name": "Debussy",
+    "email": "cdebussy@me.com",
+    "employee_id": "0000008",
+    "date_created": datetime.datetime.utcnow()
+
+}
+
+user_id = db.users.insert_one(user).inserted_id
+
+print(user_id)
+
+pprint.pprint(db.users.find_one({"employee_id": "0000008"}))
